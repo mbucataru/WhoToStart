6fece542a0d9e79a248303f932da5cff4d7dcd45
mbucataru <mbucatarugithub@gmail.com>
2025-12-29 08:56:12 -0500

    Add wireframing for Scraper

---
+++ WhoToStart.Services/IScraperService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhoToStart.Services
+{
+    public interface IScraperService
+    {
+        public Task ScrapeDraftSharks();
+        public Task ScrapeVegas();
+    }
+}
