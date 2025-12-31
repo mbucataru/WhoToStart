5edbb2eb3c82ccf4a2acf55d68150c98106b3457
mbucataru <mbucatarugithub@gmail.com>
2025-12-31 12:31:02 -0500

    Add WhoToStart.Tests

---
+++ WhoToStart.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WhoToStart.Services.Data;
+using WhoToStart.Services.Services;
+
+var services = new ServiceCollection();
+services.AddDbContext<WhoToStartDbContext>();
+services.AddScoped<IUpdaterService, UpdaterService>();
+
+var provider = services.BuildServiceProvider();
+var scraper = provider.GetRequiredService<IUpdaterService>();
+
+await scraper.UpdateProjections();
