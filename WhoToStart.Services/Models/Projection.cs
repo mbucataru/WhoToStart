de1d2f33d7089cbf2797b8625a77f7203ebf2e43
mbucataru <mbucatarugithub@gmail.com>
2026-01-02 10:19:27 -0500

    Add Missing Data Warnings to Services

--- WhoToStart.Services/Models/Projection.cs
+++ WhoToStart.Services/Models/Projection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace WhoToStart.Services.Models
@@ -18,5 +19,27 @@ namespace WhoToStart.Services.Models
         public double DraftSharksProjection { get; set; }
 
         public double FinalProjection { get; set; }
+
+        [NotMapped]
+        public string? MissingDataWarning
+        {
+            get
+            {
+                if (!IsMissingData) return null;
+
+                if (VegasProjection == 0)
+                    return "This Final Projection is higher than it should be, as there are no Vegas Projections for this player. This problem is typically resolved on Thursday.";
+
+                return "This Final Projection is lower than it should be, as there are no DraftSharks Projections for this player. This problem is typically resolved on Thursday.";
+            }
+        }
+
+        [NotMapped]
+        private bool IsMissingData
+        {
+            get => VegasProjection == 0 || DraftSharksProjection == 0;
+        }
+
+
     }
 }
